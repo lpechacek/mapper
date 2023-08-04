1fa103df1a68157ceae7ecb7a60de44470f09eac
Libor Pechacek <lpechacek@gmx.com>
2023-08-05 00:11:57 +0200

    PaintOnTemplateTool: Control antialiasing (experimental)
    The paint on template tool antialiases the drawing by default. However,
    a sharp raster is beneficial for automated processing in the color
    separation stage. This change introduces an option to control the
    antialiasing behavior of the tool.

--- src/gui/widgets/paint_on_template_settings_page.h
+++ src/gui/widgets/paint_on_template_settings_page.h
@@ -23,6 +23,7 @@
 #include <functional>
 #include <vector>
 
+#include <QCheckBox>
 #include <QObject>
 #include <QString>
 
@@ -65,6 +66,7 @@ protected:
 	QToolButton* edit_button;
 	QToolButton* move_up_button;
 	QToolButton* move_down_button;
+	QCheckBox* use_antialiasing;
 
 	void initializePage(const std::vector<QColor>& working_colors);
 	void applyPresets();
